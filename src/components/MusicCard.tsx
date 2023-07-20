e275c74efcbe156492107ab841390f4fd193199d
Gabriel Muniz <gabrielmfd@gmail.com>
2023-07-19 21:46:08 -0300

    Finaliza requisito 5

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { SongType } from '../types';
 import { StyledAudio } from '../style/StyledAudio.style';
 import { StyledAudioContainer } from '../style/StyledAudioContainer.style';
@@ -7,18 +8,39 @@ type MusicCardProps = {
 };
 
 function MusicCard({ musics }: MusicCardProps) {
-  const { trackName, previewUrl } = musics;
+  const { trackId, trackName, previewUrl } = musics;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+
   return (
     <StyledAudioContainer>
       <span>{trackName}</span>
-      <StyledAudio data-testid="audio-component" src={ previewUrl } controls>
-        <track kind="captions" />
-        O seu navegador não suporta o elemento
-        {' '}
-        {' '}
-        <code>audio</code>
-        .
-      </StyledAudio>
+      <div className="inputContainer">
+        <StyledAudio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </StyledAudio>
+        <label data-testid={ `checkbox-music-${trackId}` }>
+          <input
+            type="checkbox"
+            checked={ isChecked }
+            name="favoriteSongs"
+            onChange={ (event) => handleChecked(event) }
+          />
+          <img
+            src={ isChecked ? '/src/images/checked_heart.png'
+              : '/src/images/empty_heart.png' }
+            alt="favorite"
+          />
+        </label>
+      </div>
     </StyledAudioContainer>
   );
 }
--- src/style/StyledAudioContainer.style.tsx
+++ src/style/StyledAudioContainer.style.tsx
@@ -4,7 +4,16 @@ export const StyledAudioContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 600px;
+  width: 800px;
   color: #fff;
   margin: 5px;
+  & input {
+    width: 0;
+    margin-left: 10px;
+  }
+  .inputContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
