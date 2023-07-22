dcef7217717c3fcbdd512f0f65db50f1d8ccefd4
Gabriel Muniz <gabrielmfd@gmail.com>
2023-07-22 13:51:55 -0300

    Finaliza requisito 9

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react';
+import { RotatingLines } from 'react-loader-spinner';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+import { StyledMain } from '../style/StyledMain.style';
+import UserProfile from './UserProfile';
+
 function Profile() {
+  const [loading, setLoading] = useState(true);
+  const [loggedUser, setLoggedUser] = useState<UserType>();
+
+  useEffect(() => {
+    const userResult = async () => {
+      const userData = await getUser();
+      setLoggedUser(userData);
+      setLoading(false);
+    };
+    userResult();
+  }, []);
+
   return (
-    <p>Profile</p>
+    <StyledMain>
+      {loading && (
+        <div>
+          <h1>Carregando...</h1>
+          <br />
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="1.75"
+            width="96"
+            visible
+          />
+        </div>
+      )}
+      {!loading && (
+        <UserProfile loggedUser={ loggedUser as UserType } />
+
+      )}
+    </StyledMain>
   );
 }
 
---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import { UserType } from '../types';
+
+type UserProfileType = {
+  loggedUser: UserType;
+};
+
+function UserProfile({ loggedUser }: UserProfileType) {
+  const { description, email, image, name } = loggedUser;
+  console.log(name.length);
+  return (
+    <section>
+      <div className="image&BtnContainer">
+        <img
+          data-testid="profile-image"
+          className="profileImage"
+          src={ image }
+          alt={ name }
+        />
+        <Link className="editProfileBtn" to="/profile/edit">Editar perfil</Link>
+      </div>
+      <div className="userInfo">
+        <h3>Nome:</h3>
+        <p className="profileName">{name}</p>
+        <h3>E-mail:</h3>
+        <p className="profileEmail">{email}</p>
+        <h3>Descrição:</h3>
+        <p className="profileBio">{description}</p>
+      </div>
+    </section>
+  );
+}
+
+export default UserProfile;
